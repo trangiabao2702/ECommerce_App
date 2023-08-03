58508355a335cc0eb4a8fc80124cc281bb107a9e
trangiabao2702 <meikaruku@gmail.com>
2023-08-03 08:31:30 +0700

    add tablet responsive

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,54 +1,61 @@
-import React from 'react'
+import React from "react";
 
-import css from "./Testimonials.module.css"
-import Hero from "../../assets/testimonialHero.png"
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { TestimonialsData } from "../../data/testimonials"
+import css from "./Testimonials.module.css";
+import Hero from "../../assets/testimonialHero.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { TestimonialsData } from "../../data/testimonials";
 
 const Testimonials = () => {
   return (
     <div className={css.testimonials}>
-        <div className={css.wrapper}>
-            <div className={css.container}>
-                <span>Top Rated</span>
-                <span>
-                    Seedily say has suitable disposal and boy. Exercise joy man children rejoiced.
-                </span>
-            </div>
-
-            <img src={Hero} alt="Testimonial Hero" />
-
-            <div className={css.container}>
-                <span>100k</span>
-                <span>Happy Customers with us</span>
-            </div>
+      <div className={css.wrapper}>
+        <div className={css.container}>
+          <span>Top Rated</span>
+          <span>Seedily say has suitable disposal and boy. Exercise joy man children rejoiced.</span>
         </div>
 
-        <div className={css.reviews}>Reviews</div>
+        <img src={Hero} alt="Testimonial Hero" />
 
-        <div className={css.carousel}>
-            <Swiper
-            slidesPerView={3}
-            slidesPerGroup={1}
-            spaceBetween={20}
-            className={css.tCarousel}
-            >
-                {
-                    TestimonialsData.map((testimonial, i) => (
-                        <SwiperSlide key={i}>
-                            <div className={css.testimonial}>
-                                <img src={testimonial.image} alt="Testimonial" />
-                                <span>{testimonial.comment}</span>
-                                <hr/>
-                                <span>{testimonial.name}</span>
-                            </div>
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+        <div className={css.container}>
+          <span>100k</span>
+          <span>Happy Customers with us</span>
         </div>
+      </div>
+
+      <div className={css.reviews}>Reviews</div>
+
+      <div className={css.carousel}>
+        <Swiper
+          slidesPerView={3}
+          slidesPerGroup={1}
+          spaceBetween={20}
+          className={css.tCarousel}
+          breakpoints={{
+            856: {
+              slidesPerView: 3,
+            },
+            640: {
+              slidesPerView: 2,
+            },
+            0: {
+              slidesPerView: 1,
+            },
+          }}
+        >
+          {TestimonialsData.map((testimonial, i) => (
+            <SwiperSlide key={i}>
+              <div className={css.testimonial}>
+                <img src={testimonial.image} alt="Testimonial" />
+                <span>{testimonial.comment}</span>
+                <hr />
+                <span>{testimonial.name}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CgShoppingBag } from "react-icons/cg";
+import { FaBars } from "react-icons/fa";
 
-import css from "./Header.module.css"
-import Logo from "../../assets/logo.png"
-import { CgShoppingBag } from "react-icons/cg"
+import css from "./Header.module.css";
+import Logo from "../../assets/logo.png";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(true);
+
+  const toggleMenu = () => {
+    setShowMenu((showMenu) => !showMenu);
+  };
+
   return (
     <div className={css.container}>
-        <div className={css.logo}>
-            <img src={Logo} alt="Logo" />
-            <span>Amazon</span>
+      <div className={css.logo}>
+        <img src={Logo} alt="Logo" />
+        <span>Amazon</span>
+      </div>
+
+      <div className={css.right}>
+        <div className={css.bars} onClick={toggleMenu}>
+          <FaBars />
         </div>
 
-        <div className={css.right}>
-            <div className={css.menu}>
-                <ul className={css.menu}>
-                    <li>Collections</li>
-                    <li>Brands</li>
-                    <li>New</li>
-                    <li>Sales</li>
-                    <li>ENG</li>
-                </ul>
-            </div>
-            
-            <input type="text" className={css.search} placeholder="Search" />
+        <ul className={css.menu} style={{ display: showMenu ? "inherit" : "none" }}>
+          <li>Collections</li>
+          <li>Brands</li>
+          <li>New</li>
+          <li>Sales</li>
+          <li>ENG</li>
+        </ul>
 
-            <CgShoppingBag className={css.cart} />
-        </div>
+        <input type="text" className={css.search} placeholder="Search" />
+
+        <CgShoppingBag className={css.cart} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
