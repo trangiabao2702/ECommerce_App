4636afa1d5863326e7bfabdbb5e17109fc87a63b
trangiabao2702 <meikaruku@gmail.com>
2023-08-03 07:30:48 +0700

    add animation for hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,56 +1,59 @@
-import React from 'react'
+import React from "react";
+import { RiShoppingBagFill } from "react-icons/ri";
+import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
 
-import css from "./Hero.module.css"
-import HeroImg from "../../assets/hero.png"
-import { RiShoppingBagFill } from "react-icons/ri"
-import { BsArrowRight } from "react-icons/bs"
+import css from "./Hero.module.css";
+import HeroImg from "../../assets/hero.png";
 
 const Hero = () => {
+  const transition = { duration: 3, type: "spring" };
+
   return (
     <div className={css.container}>
-        {/* left side */}
-        <div className={css.h_sides}>
-            <span className={css.text1}>Skin protection cream</span>
+      {/* left side */}
+      <div className={css.h_sides}>
+        <span className={css.text1}>Skin protection cream</span>
 
-            <div className={css.text2}>
-                <span>Trendy Collection</span>
-                <span>Seedily say has suitable disposal and boy. Exercise joy man children rejoiced.</span>
+        <div className={css.text2}>
+          <span>Trendy Collection</span>
+          <span>Seedily say has suitable disposal and boy. Exercise joy man children rejoiced.</span>
+        </div>
+      </div>
+
+      {/* middle side */}
+      <div className={css.wrapper}>
+        <motion.div initial={{ bottom: "2rem" }} whileInView={{ bottom: "0rem" }} className={css.blueCircle} transition={transition}></motion.div>
+
+        <img initial={{ bottom: "-2rem" }} whileInView={{ bottom: "0rem" }} transition={transition} src={HeroImg} alt="Hero" width={600} />
+
+        <div initial={{ right: "4%" }} whileInView={{ right: "2%" }} transition={transition} className={css.cart2}>
+          <RiShoppingBagFill />
+
+          <div className={css.signup}>
+            <span>Best Signup Offers</span>
+
+            <div>
+              <BsArrowRight />
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* right side */}
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span>1.5m</span>
+          <span>Monthly Traffic</span>
         </div>
 
-
-        {/* middle side */}
-        <div className={css.wrapper}>
-            <div className={css.blueCircle}></div>
-            <img src={HeroImg} alt="Hero" width={600} />
-            <div className={css.cart2}>
-                <RiShoppingBagFill />
-
-                <div className={css.signup}>
-                    <span>Best Signup Offers</span>
-
-                    <div>
-                        <BsArrowRight />
-                    </div>
-                </div>
-            </div>
+        <div className={css.customers}>
+          <span>100k</span>
+          <span>Happy Customers</span>
         </div>
-
-        
-        {/* right side */}
-        <div className={css.h_sides}>
-            <div className={css.traffic}>
-                <span>1.5m</span>
-                <span>Monthly Traffic</span>
-            </div>
-
-            <div className={css.customers}>
-                <span>100k</span>
-                <span>Happy Customers</span>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
